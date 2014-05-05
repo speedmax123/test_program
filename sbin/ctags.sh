e45cd08220133c74a140566f73d02e95a379d71e
speedmax123 <xjttt1991@gmail.com>
2014-05-06 01:36:58 +0800

    新文件：    log/ctags.log 	新文件：    sbin/ctags.sh 	新文件：    src/FILE_DIR_2/Makefile 	新文件：    src/FILE_DIR_2/a.out 	新文件：    src/FILE_DIR_2/test_type 	新文件：    src/FILE_DIR_2/types.c 	新文件：    test_tags

---
+++ sbin/ctags.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+TEST_HOME="/home/speedmax/code/test_program"
+LOG_DIR="$TEST_HOME/log"
+LOG="ctags.log"
+
+cd $TEST_HOME
+ctags -f $TEST_HOME/test_tags -R>>$LOG_DIR/$LOG 2>$LOG_DIR/$LOG
+
+
