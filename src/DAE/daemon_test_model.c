e1fbb703ce0de236bfeb172ba47ffc0d61e2e011
speedmax <xjttt1991@gmail.com>
2014-09-28 00:23:47 +0800

    修改:         Makefile 	修改:         daemon_test_model.c

--- src/DAE/daemon_test_model.c
+++ src/DAE/daemon_test_model.c
@@ -1,7 +1,10 @@
 #include "apue.h"
+#include <stdio.h>
+#include <unistd.h>
 #include <syslog.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <sys/time.h>
 #include <sys/resource.h>
 
 void
@@ -11,4 +14,65 @@ daeomnize(const char *cmd) {
     struct rlimit    rl;
     struct sigaction sa;
 
+//clear file creation mask
+    umask(0);
+//get the maxium number of file descriptors
+    if (getrlimit(RLIMIT_NOFILE, &rl) < 0) {
+        err_quit("%s: can't get file limit", cmd);
+    }
+//become a session leader to lose control of TTY
+    if ((pid = fork()) < 0) {
+        err_quit("%s: can not fork", cmd);
+    }
+    else if (pid != 0) /* parent */{
+        exit(0);
+    }
+    setsid();
+//enable futhure open won't allocate new TTYs
+    sa.sa_handler = SIG_IGN;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    if (sigaction(SIGHUP, &sa, NULL) < 0)
+        err_quit("%s: can't ignore SIGHUP");
+    if ((pid = fork()) < 0) {
+        err_quit("%s: can't fork", cmd);
+    }
+    else if (pid == 0) {
+        /* parent */
+        exit(0);
+    }
+//change the current working directory to the root so we don't prevent file system from being unmounted
+    if (chdir("/") < 0) {
+        err_quit("%s: can't change directory to /");
+    }
+//close all file descriptors
+    if (rl.rlim_max == RLIM_INFINITY)
+        rl.rlim_max = 1024;
+    for (i = 0; i < rl.rlim_max; i++) {
+        close(i);
+    }
+//Attach file descriptor 0, 1, 2 to /dev/null
+    fd0 = open("/dev/null", O_RDWR);
+    fd1 = dup(0);
+    fd2 = dup(0);
+//initilize the log file
+    openlog(cmd, LOG_CONS, LOG_DAEMON);
+    if (fd0 != 0 || fd1 != 1 || fd2 != 2) {
+        syslog(LOG_ERR, "expected file descriptor %d %d %d", fd0, fd1, fd2);
+        exit(1);
+    }
+}
+
+int
+main(int argc, char *argv[]) {
+    /*
+    if (argc != 2) {
+        err_quit("%s: ./daemon_stest_model.out CMD", "usage");
+    }
+    else {
+        printf("current command is %s\n", argv[1]);
+    }
+    */
+    daeomnize("");
+    return(0);
 }
